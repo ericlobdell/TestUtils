03771a996bd13137339ff517abced477e3f59c88
Eric Lobdell <eric@ericlobdell.me>
2017-04-09 18:45:52 -0400

    initial commit

---
+++ TestUtils.Tests/RequestDataBuilderTests.cs
@@ -0,0 +1,65 @@
+﻿using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace TestUtils.Tests
+{
+  public class RequestDataBuilderTests
+  {
+    [Fact]
+    public void Set_sets_property_value()
+    {
+      var sut = new RequestDataBuilder<Person>()
+        .Set(p => p.Name, "Eric")
+        .Set(p => p.Age, 12)
+        .Create();
+
+      Assert.True(sut.ContainsKey("Name"));
+      Assert.True(sut.ContainsKey("Age"));
+    }
+
+    [Fact]
+    public void Default_ctor_sets_no_values()
+    {
+      var sut = new RequestDataBuilder<Person>()
+        .Create();
+
+      Assert.True(sut.Count == 0);
+    }
+
+    [Theory, AutoData]
+    public void Template_ctor_maps_property_values_from_template(Person template)
+    {
+      var sut = new RequestDataBuilder<Person>(template)
+        .Create();
+
+      Assert.Equal(sut["Name"], template.Name);
+      Assert.Equal(sut["Age"], template.Age);
+    }
+
+    [Theory, AutoData]
+    public void Set_overwrites_property_values_from_template(Person template)
+    {
+      var sut = new RequestDataBuilder<Person>()
+        .Set(p => p.Name, "Eric")
+        .Create();
+
+      Assert.NotEqual(sut["Name"], template.Name);
+    }
+
+    [Theory, AutoData]
+    public void Omit_removes_properties(Person template)
+    {
+      var sut = new RequestDataBuilder<Person>(template)
+        .Omit(p => p.Age)
+        .Create();
+
+      Assert.False(sut.ContainsKey("Age"));
+    }
+  }
+
+  public class Person
+  {
+    public string Name { get; set; }
+    public int Age { get; set; }
+  }
+}
---
+++ TestUtils/RequestDataBuilder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace TestUtils
+{
+  public class RequestDataBuilder<T> : IAnonymousDataBuilder<T>
+    where T : new()
+  {
+    private Dictionary<string, object> data;
+
+    public RequestDataBuilder()
+    {
+      data = new Dictionary<string, object>();
+    }
+
+    public RequestDataBuilder(T template)
+    {
+      var props = template.GetType().GetProperties();
+      data = new Dictionary<string, object>();
+
+      foreach (var prop in props)
+        data.Add(prop.Name, GetPropValue(template, prop.Name));
+    }
+
+    public Dictionary<string, object> Create()
+    {
+      return data;
+    }
+
+    public IAnonymousDataBuilder<T> Omit<TKey>(Expression<Func<T, TKey>> keySelector)
+    {
+      var propertyName = GetPropertyName(keySelector);
+
+      if (data.ContainsKey(propertyName))
+        data.Remove(propertyName);
+
+      return this;
+    }
+
+    public IAnonymousDataBuilder<T> Set<TKey>(Expression<Func<T, TKey>> keySelector, object value)
+    {
+      var propertyName = GetPropertyName(keySelector);
+
+      if (data.ContainsKey(propertyName))
+        data[propertyName] = value;
+      else
+        data.Add(propertyName, value);
+
+      return this;
+    }
+
+    private string GetPropertyName<TKey>(Expression<Func<T, TKey>> keySelector)
+    {
+      return (keySelector.Body as MemberExpression).Member.Name;
+    }
+
+    public static object GetPropValue(object src, string propName)
+    {
+      return src.GetType().GetProperty(propName).GetValue(src, null);
+    }
+  }
+  public interface IAnonymousDataBuilder<T>
+  {
+    IAnonymousDataBuilder<T> Set<TKey>(Expression<Func<T, TKey>> keySelector, object value);
+    IAnonymousDataBuilder<T> Omit<TKey>(Expression<Func<T, TKey>> keySelector);
+
+    Dictionary<string, object> Create();
+  }
+}
